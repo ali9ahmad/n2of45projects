e243837c167314a8e1eeba2ff5ade4aa6fc7b38d
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-06-27 21:55:46 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello worls and happy hacking.
         </p>
         <a
           className="App-link"
